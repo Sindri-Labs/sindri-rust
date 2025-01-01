e6a8037d501b2ac8a26108e00af43bc39e52c958
Katie McKeon <katiemckeonphd@gmail.com>
2025-01-01 22:45:39 +0000

    commentary

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -12,9 +12,9 @@ cat openapi.json | jq -r '.content' | base64 -d | jq '.' > openapi_decoded.json
 # Generate the client
 npx @openapitools/openapi-generator-cli generate -i openapi_decoded.json -g rust -o ../openapi --additional-properties=supportMiddleware=true
 
-# Patch
+# Patch over the generated client with some manual changes
 cd .. && git apply openapi/openapi.patch && cd scripts
 
-# Remove the spec
+# Remove the spec files
 rm openapi.json
 rm openapi_decoded.json
