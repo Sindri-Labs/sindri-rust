40618af16415dd86e81ff4ebc10015434779cd1b
Katie McKeon <katiemckeonphd@gmail.com>
2025-02-25 19:06:37 +0000

    try most recent generator

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -3,19 +3,11 @@
 # Use the location of the script as the working directory.
 cd "$(dirname "$0")"
 
-# Download the open api spec that has been downgraded to v3.0.3
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  -O \
-  -L https://api.github.com/repos/Sindri-Labs/sindri-labs.github.io/contents/docs/reference/api/openapi.json
-
-# Decode the base64 encoded content 
-cat openapi.json | jq -r '.content' | base64 -d | jq '.' > openapi_decoded.json
+# Download the most recent open api spec
+wget https://sindri.app/api/openapi.json
 
 # Generate the client
-npx @openapitools/openapi-generator-cli@2.15.3 generate -i openapi_decoded.json -g rust -o ../openapi --additional-properties=supportMiddleware=true
+npx @openapitools/openapi-generator-cli@2.16.3 generate -i openapi.json -g rust -o ../openapi --additional-properties=supportMiddleware=true
 
 # Move up to the project root.
 cd ..
@@ -24,9 +16,8 @@ cd ..
 rustfmt $(find ./openapi/ -name "*.rs")
 
 # Patch over the generated client with some manual changes
-git apply openapi/openapi.patch
+# git apply openapi/openapi.patch
 
 # Move back into scripts and remove the spec files.
 cd ./scripts/
 rm openapi.json
-rm openapi_decoded.json
