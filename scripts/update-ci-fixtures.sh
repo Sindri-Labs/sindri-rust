1beb3513e89c50c6adb191ce7c6806bd43784e68
Katie McKeon <62887234+katiemckeon@users.noreply.github.com>
2025-03-12 17:29:47 -0700

    Chore: upgrade internal client + remove UA in fixtures (#23)
    * script updates
    
    * overdue lintfix for internal api
    
    * make recording openapi version independent

--- scripts/update-ci-fixtures.sh
+++ scripts/update-ci-fixtures.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e  # Exit on error
 
+# Use the location of the script as the working directory.
+cd "$(dirname "$0")"
+
 # Load api key
 if [ -f "../.env" ]; then
     export $(cat ../.env | xargs)
     echo ".env file found and loaded"
 fi
 
-# Set VCR_PATH environment variable
+# Set VCR_PATH environment variable and remove old recording
 export VCR_PATH="../.github/assets/recordings/integration.vcr.json" 
+rm -f "$VCR_PATH"
 
 # Run test in record mode
 cargo test --package sindri --features record --test offline -- end_to_end
