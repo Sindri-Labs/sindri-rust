1f7108976aca558e832bb6d434be1b3f9e4aa0da
Katie McKeon <katiemckeonphd@gmail.com>
2025-04-04 19:39:24 +0000

    make terminal output more consistent with other cms

--- cli/src/commands/login.rs
+++ cli/src/commands/login.rs
@@ -8,7 +8,7 @@ pub fn login(client: &SindriClient, username: Option<String>, password: Option<S
     let username = match username {
         Some(u) => u,
         None => Input::new()
-            .with_prompt("Username")
+            .with_prompt("  Username")
             .interact_text()
             .unwrap_or_else(|e| handle_operation_error("Login", &e.to_string())),
     };
@@ -16,13 +16,13 @@ pub fn login(client: &SindriClient, username: Option<String>, password: Option<S
     let password = match password {
         Some(p) => p,
         None => Password::new()
-            .with_prompt("Password")
+            .with_prompt("  Password")
             .interact()
             .unwrap_or_else(|e| handle_operation_error("Login", &e.to_string())),
     };
 
     let name = Input::new()
-        .with_prompt("New API Key Name")
+        .with_prompt("  New API Key Name")
         .with_initial_text(format!("{}-rust-sdk", username))
         .validate_with(|input: &String| -> Result<(), String> {
             if input.len() > 32 {
@@ -40,6 +40,7 @@ pub fn login(client: &SindriClient, username: Option<String>, password: Option<S
         Ok(token) => token,
         Err(e) => handle_operation_error("Login", &e.to_string()),
     };
+    println!("{}", console::style("  ✓ Valid credentials").cyan());
 
     // Collect list of teams for the user
     let teams = match rt.block_on(client.teams_jwt_auth(&token)) {
@@ -53,13 +54,11 @@ pub fn login(client: &SindriClient, username: Option<String>, password: Option<S
     // Let user select a team
     let team_names: Vec<String> = teams.iter().map(|t: &TeamDetail| t.slug.clone()).collect();
     let selection = Select::new()
-        .with_prompt("Select a team to generate an API key for")
+        .with_prompt("  Select a team to generate an API key for")
         .items(&team_names)
         .interact()
         .unwrap_or_else(|e| handle_operation_error("Login", &e.to_string()));
     let selected_team = &teams[selection];
-
-
 
     // Generate API key for selected team
     let api_key = match rt.block_on(client.api_key_select_team(
@@ -73,15 +72,25 @@ pub fn login(client: &SindriClient, username: Option<String>, password: Option<S
     };
 
     println!(
+        "{}",
+        console::style("  ✓ API key generated successfully!").cyan()
+    );
+
+    println!(
         "\n{}",
-        console::style("API key generated successfully!")
-            .green()
-            .bold()
+        console::style("To authenticate future requests with the rust SDK, you can either:").bold()
     );
     println!(
-        "{}",
-        console::style("Please copy this key as it will only be shown once:").bold()
+        "• Set SINDRI_API_KEY={} in your environment variables",
+        console::style(&api_key).cyan()
     );
-    println!("{}", console::style(api_key).cyan());
-    println!("\n{}", console::style("You can now use this key with the --api-key flag or set `SINDRI_API_KEY` in your environment variables.").dim());
+    println!(
+        "• Supply --api-key={} in any `cargo sindri` command",
+        console::style(&api_key).cyan()
+    );
+
+    println!(
+        "\n{}",
+        console::style("Make sure to keep track of this key as it will only be shown once").dim()
+    );
 }
